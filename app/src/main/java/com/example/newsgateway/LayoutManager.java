894d0309f0cec94c307d322ec01ce79ca9655425
Akash Tanwani <atanwani@hawk.iit.edu>
2021-01-03 00:53:13 -0600

    functional code uploaded

---
+++ app/src/main/java/com/example/newsgateway/LayoutManager.java
@@ -0,0 +1,46 @@
+package com.example.newsgateway;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LayoutManager implements Serializable {
+
+    private int source;
+    private int article;
+    private List<String> categories = new ArrayList<>();
+    private List<NewsSource> sources = new ArrayList<>();
+    private List<NewsArticle> articles = new ArrayList<>();
+
+    public void setSource(int source) {
+        this.source = source;
+    }
+
+    public void setArticle(int article) {
+        this.article = article;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public List<NewsSource> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<NewsSource> sources) {
+        this.sources = sources;
+    }
+
+    public List<NewsArticle> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<NewsArticle> articles) {
+        this.articles = articles;
+    }
+}
